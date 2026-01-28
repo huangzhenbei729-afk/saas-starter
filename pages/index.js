79ea33b0842e000d12ca3ca97e90ebc475158aa3
自建阿根廷 <huangzhenbei729@gmail.com>
2026-01-28 15:15:30 -0300

    Initial commit of Next.js SaaS boilerplate template

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>SaaS Starter</title>
+      </Head>
+      <h1>Welcome to the SaaS Starter</h1>
+    </div>
+  );
+}
